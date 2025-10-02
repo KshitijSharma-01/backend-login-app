282ff12700825328388f1f2174cc55fc9137257c
kshitij sharma <kshitijsharma1152004@gmail.com>
2025-10-02 12:41:16 +0530

    Update Signup.js

--- frontend/src/Pages/Signup.js
+++ frontend/src/Pages/Signup.js
@@ -19,7 +19,7 @@ const Signup = () => {
             return handleError("name, email and password is required")
         }
         try {
-            const url = "http://localhost:8000/auth/signup"
+            const url = "https://backend-login-app-api1.vercel.app/auth/signup"
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
@@ -99,5 +99,6 @@ const Signup = () => {
         </div>
     )
 }
+
 
 export default Signup
